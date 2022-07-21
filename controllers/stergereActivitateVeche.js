6e124c48f4e857f9dd066b50fb848bad1f060754
MariusWik <Petre.marius@gmail.com>
2022-07-21 21:45:38 +0300

    dsada

--- controllers/stergereActivitateVeche.js
+++ controllers/stergereActivitateVeche.js
@@ -1,24 +1,21 @@
 const handleStergereActivitateVeche = (db) => (req, res) => {
-    const { idstergere} = req.body;
-    
-   
-    const numedb=`activitatidirectievechi`;
-    const idsters=`${idstergere}`;
-    
-    db.transaction((trx) => {
-      trx
-        .delete()
-        .from(numedb)
-        .where('id', '=', `${idsters}`)
-        .then(trx.commit)
-        .catch(trx.rollback);
-    }).catch((err) => res.status(400).json(err));
+  const { idstergere } = req.body;
 
-    res.json('Activitate stearsa');
-   
-  };
+  const numedb = `activitatidirectievechi`;
+  const idsters = `${idstergere}`;
 
-  
-  
-  module.exports = {
-    handleStergereActivitateVeche: handleStergereActivitateVeche}
+  db.transaction((trx) => {
+    trx
+      .delete()
+      .from(numedb)
+      .where("id", "=", `${idsters}`)
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json(err));
+
+  res.json("Activitate stearsa");
+};
+
+module.exports = {
+  handleStergereActivitateVeche: handleStergereActivitateVeche,
+};
