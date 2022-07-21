8ddec02c7d27745a4ccd9788ef4184ea82256c38
MariusWik <Petre.marius@gmail.com>
2022-07-21 21:39:47 +0300

    re

--- controllers/register.js
+++ controllers/register.js
@@ -1,29 +1,24 @@
 const handleRegister = (db, bcrypt) => (req, res) => {
-    const { username, password, sector} = req.body;
-    if (!username || !password || !sector){
-       return  res.status(400).json('incorrect form subbmision')
-    }
-    const hash = bcrypt.hashSync(password);
-  
-    db.transaction((trx) => {
-      trx
-        .insert({
-          hash: hash,
-          username: username,
-          sector: sector
-        })
-        .into("login")
-        
-        .catch((err) => res.status(400).json("Utilizatorul exista"))
-        .then(trx.commit)
-    })
+  const { username, password, sector } = req.body;
+  if (!username || !password || !sector) {
+    return res.status(400).json("incorrect form subbmision");
+  }
+  const hash = bcrypt.hashSync(password);
 
-    
-  };
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        username: username,
+        sector: sector,
+      })
+      .into("login")
 
-  
-  
-  module.exports = {
-    handleRegister: handleRegister,
-  };
-  
+      .catch((err) => res.status(400).json("Utilizatorul exista"))
+      .then(trx.commit);
+  });
+};
+
+module.exports = {
+  handleRegister: handleRegister,
+};
