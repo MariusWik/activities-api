41bd9f14c013061d1be905992d9f73c514cf5b74
MariusWik <Petre.marius@gmail.com>
2022-07-21 20:40:39 +0300

    conne string

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const stergereActivitateVeche = require("./controllers/stergereActivitateVeche.j
 const db = knex({
     client: "pg",
     connection: {
-      host : process.env.DATABASE_URL,
+      connectionString : process.env.DATABASE_URL,
     ssl:true
       
     }, 
