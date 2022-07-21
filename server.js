7a952fc672c66497d965270dcab100bc09009365
MariusWik <Petre.marius@gmail.com>
2022-07-21 21:09:49 +0300

    rest

--- server.js
+++ server.js
@@ -29,7 +29,13 @@ const db = knex({
     }, 
   });
 
-
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+ });
   app.use(bodyParser.json());
 app.use(cors());
 app.get('/', (req, res)=>res.send('merge'));
