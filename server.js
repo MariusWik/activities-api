f319c0e41db94d740710c963320bf208b3227355
MariusWik <Petre.marius@gmail.com>
2022-07-22 06:47:35 +0300

    app

--- server.js
+++ server.js
@@ -32,11 +32,11 @@ const db = knex({
 
   
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://activitatidirectie.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-  
+
   app.use(cors);
 
   app.use(bodyParser.json());
