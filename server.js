98be3fc2e58143df6206f798ca143f75751dbd06
MariusWik <Petre.marius@gmail.com>
2022-07-22 20:15:08 +0300

    corsoption

--- server.js
+++ server.js
@@ -28,20 +28,22 @@ const db = knex({
       
     }, 
   });
- 
- app.use(cors({origin:'https://activitatidirectie.herokuapp.com'}));
+ var corsOption = {
+  origin: 'https://activitatidirectie.herokuapp.com'
+ }
+ app.use(cors());
 
   app.use(bodyParser.json());
 
 app.get('/', (req, res)=>res.send('merge'));
-app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/signin",  signin.handleSignin(db, bcrypt));
 app.post("/register", register.handleRegister(db, bcrypt));
 app.put("/stergereactivitate", stergereActivitate.handleStergereActivitate(db));
 app.post("/modificareactivitate", modificareActivitate.handleModificareActivitate(db));
 app.post("/adaugareativitate", adaugareActivitate.handleAdaugareActivitate(db));
 app.post("/afisareactivitati", afisareActivitati.handleAfisareActivitati(db));
-app.get("/afisaretoate", afisareToate.handleAfisareToate(db));
-app.get("/afisareuseri", afisareUseri.handleAfisareUseri(db));
+app.get("/afisaretoate",cors(corsOption), afisareToate.handleAfisareToate(db));
+app.get("/afisareuseri",cors(corsOption), afisareUseri.handleAfisareUseri(db));
 app.put("/stergereuser", stergereUser.handleStergereUser(db));
 app.post("/schimbareparola", schimbareParola.handleSchimbareParola(db, bcrypt));
 app.post("/arhivareactivitate", arhivareActivitate.handleArhivareActivitate(db));
