6ac684605e7bc24a81d387f088f841a176208758
MariusWik <Petre.marius@gmail.com>
2022-07-23 17:48:17 +0300

    res

--- controllers/afisareUseri.js
+++ controllers/afisareUseri.js
@@ -2,7 +2,7 @@ const handleAfisareUseri = (db) => (req,res) => {
   
   const numedb = "login";
   
-  db.select('id','username', 'sector').from(numedb).orderBy('sector', 'asc').then(results => res.send(results));
+  db.select('id','username', 'sector').from(numedb).orderBy('sector', 'asc').then(results => res.jeson(results));
 }
 
 module.exports = {
