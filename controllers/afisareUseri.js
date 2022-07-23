1fd703e432a4e14bcc2d5ad76089bf0af648d06e
MariusWik <Petre.marius@gmail.com>
2022-07-23 17:33:17 +0300

    m

--- controllers/afisareUseri.js
+++ controllers/afisareUseri.js
@@ -1,7 +1,7 @@
 const handleAfisareUseri = (db) => (req,res) => {
   
   const numedb = "login";
-
+  res.setHeader('Access-Control-Allow-Origin', 'https://activitatidirectie.herokuapp.com');
   db.select('id','username', 'sector').from(numedb).orderBy('sector', 'asc').then(results => res.send(results));
 }
 
