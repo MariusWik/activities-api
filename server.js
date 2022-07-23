516790099348e48eff7a063aac4d3f018f9f13ab
MariusWik <Petre.marius@gmail.com>
2022-07-23 17:38:06 +0300

    d

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://activitatidirectie.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://activitatidirectie.herokuapp.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
