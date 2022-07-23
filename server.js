de5ffafcda60676f8d154973c76e4733fb0510c5
MariusWik <Petre.marius@gmail.com>
2022-07-23 17:53:28 +0300

    cors

--- server.js
+++ server.js
@@ -29,27 +29,8 @@ const db = knex({
 });
 
 const app = express();
-app.use(cors());
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://activitatidirectie.herokuapp.com');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 app.use(bodyParser.json());
-
+app.use(cors());
 //app.get('/', (req, res)=>res.send('merge'));
 app.post('/signin',  signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
