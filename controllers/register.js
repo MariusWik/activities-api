0a10687dc6837514a7ac8557beb37aca08433157
MariusWik <Petre.marius@gmail.com>
2022-07-26 21:54:40 +0300

    whereExists

--- controllers/register.js
+++ controllers/register.js
@@ -6,7 +6,7 @@ const handleRegister = (db, bcrypt) => (req, res) => {
 
   const hash = bcrypt.hashSync(password);
   const numedb = "login";
-  const exista = db.select('*').exists(
+  const exista = db.whereExists(
     db.select("1").from(numedb).where("username", "=", `${username}`)
   );
 
