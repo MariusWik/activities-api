726fb26cd31490c10c46a4fd484b732281189d94
MariusWik <Petre.marius@gmail.com>
2022-07-26 21:23:27 +0300

    res

--- controllers/register.js
+++ controllers/register.js
@@ -3,20 +3,32 @@ const handleRegister = (db, bcrypt) => (req, res) => {
   if (!username || !password || !sector) {
     return res.status(200).json("Nu ati introdus corect datele");
   }
+
   const hash = bcrypt.hashSync(password);
   const numedb='login';
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        username: username,
-        sector: sector,
-      })
-      .into(numedb)
+  const exista = db.select('*').from(numedb).where('sector','=',`${nrsector}`).orderBy('id', 'desc').then(results => res.send(results));
 
-      .catch((err) => res.status(200).json("Utilizatorul exista"))
-      .then(trx.commit);
-  } ) 
+
+if (exista===0) {db.transaction((trx) => {
+  trx
+    .insert({
+      hash: hash,
+      username: username,
+      sector: sector,
+    })
+    .into(numedb)
+    .then(trx.commit)
+    .catch(trx.rollback);
+    res.status(200).json('utilizator introdus');
+} ) } else {
+   res.status(200).json('utilizatorul exista')
+}
+  
+
+  
+
+
+  
   
 };
 
