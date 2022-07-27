b2290c3446c9f1b50ca7562b8e2f67c3583f4475
MariusWik <Petre.marius@gmail.com>
2022-07-27 07:09:37 +0300

    reg

--- controllers/register.js
+++ controllers/register.js
@@ -3,9 +3,14 @@ const handleRegister = (db, bcrypt) => (req, res) => {
   if (!username || !password || !sector) {
     return res.status(200).json("Nu ati introdus corect datele");
   }
-
   const hash = bcrypt.hashSync(password);
+
   const numedb = "login";
+
+const exista= db.select(db.exists(db.select('1').from(numedb.where('username','=',`${username}`))))
+console.log(exista);
+
+
   db.transaction((trx) => {
     trx
       .insert({
