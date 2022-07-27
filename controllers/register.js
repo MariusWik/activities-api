373107a7a2ceda5370997b1693c188f176c5d718
MariusWik <Petre.marius@gmail.com>
2022-07-27 20:15:39 +0300

    s

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,4 @@
-const handleRegister = (db, bcrypt) =>  (req, res) => {
+const handleRegister = (db, bcrypt) => (req, res) => {
   const { username, password, sector } = req.body;
   if (!username || !password || !sector) {
     return res.status(200).json("Nu ati introdus corect datele");
@@ -7,28 +7,7 @@ const handleRegister = (db, bcrypt) =>  (req, res) => {
   const hash = bcrypt.hashSync(password);
   const numedb = "login";
 
-
-  try {
   db.transaction((trx) => {
-   trx
-    .insert({
-      hash: hash,
-      username: username,
-      sector: sector,
-    })
-    .into(numedb)
-    
-    .then(trx.commit)
-  });} catch(err){
-   if (err) {return res.status(200).json('Utilizatorul exista')} 
-  } 
-   
-      res.status(200).json('Utilizator introdus')
-  
-
-
-
-/*   db.transaction((trx) => {
     trx
       .insert({
         hash: hash,
@@ -37,10 +16,10 @@ const handleRegister = (db, bcrypt) =>  (req, res) => {
       })
       .into(numedb)
 
-      .catch((err) => res.status(400).json("Utilizatorul exista"))
+      .catch((err) => res.json(err))
       .then(trx.commit);
-  } ) 
-  res.status(200).json('Utilizator introdus'); */
+  });
+  res.status(200).json("Utilizator introdus");
 };
 
 module.exports = {
