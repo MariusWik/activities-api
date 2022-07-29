32030209156af391cfcd011b2f153955871c08e1
MariusWik <Petre.marius@gmail.com>
2022-07-29 17:51:13 +0300

    copiere

--- controllers/copiereActivitate.js
+++ controllers/copiereActivitate.js
@@ -7,10 +7,17 @@ const handleCopiereActivitate = (db) => (req, res) => {
     
     db.transaction((trx) => {
       trx
-        .insert(db.select( `${sector}`,'activitate', 'datastart','datastop','observatii').from(numedb2).where('id','=',`${idCopiat}`))
-        .into(numedb( `${sector}`,'activitate', 'datastart', 'datastop','observatii'))
+        .insert({
+          sector: db2.select(`${sector}`). from(numedb2).where('id','=',`${idCopiat}`),
+          activitate: db2.select(`activitate`). from(numedb2).where('id','=',`${idCopiat}`),
+          datastart: db2.select(`datastart`). from(numedb2).where('id','=',`${idCopiat}`),
+          datastop: db2.select(`datastop`). from(numedb2).where('id','=',`${idCopiat}`),
+          procent: db2.select(`procent`). from(numedb2).where('id','=',`${idCopiat}`),
+          observatii:  db2.select(`observatii`). from(numedb2).where('id','=',`${idCopiat}`),
+        })
+        .into(numedb)
         .then(trx.commit)
-        .catch(trx.rollback);
+        .catch(trx.rollback)
     })
    
   
